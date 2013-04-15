b04fda64cdf026295ce0d647650f55b15f87ee37
smagch <smagch@gmail.com>
2013-04-15 17:26:01 +0900

    add demos

---
+++ demo/files/config.php
@@ -0,0 +1,5 @@
+<?php
+$GLOBAL['config'] = array(
+    'foo' => 'bar'
+);
+echo 'config loaded';
---
+++ demo/foobar/lib/FooBar.php
@@ -0,0 +1,11 @@
+<?php
+
+class Foo
+{
+    public $foo = 'foo';
+}
+
+class Bar
+{
+    public $bar = 'bar';
+}
